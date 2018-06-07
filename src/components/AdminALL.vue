a64b1d0cbbdd09731bf1fbf7fb2c842e2289183b
周昊 <zhouhao1@corp.netease.com>
2018-06-07 14:45:16 +0800

    引入table

--- src/components/AdminALL.vue
+++ src/components/AdminALL.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <div v-for="example of examples" :key="example.id" @click="goToAdmin(example.id)">
-      <span>{{example.id}}</span>--<span>{{example.title}}</span>--<span>{{example.image}}</span>
-    </div>
+  <div class="table-container">
+    <h1>EXAMPLES</h1>
+    <el-table :data="examples" border style="width: 100%" @row-click="goToAdmin">
+      <el-table-column prop="id" width=50 label="id" align="center"></el-table-column>
+      <el-table-column prop="title" label="标题" align="center"></el-table-column>
+      <el-table-column prop="image" label="图片" align="center"></el-table-column>
+    </el-table>
   </div>
 </template>
 
@@ -38,14 +41,21 @@
             console.log('查询example错误', e)
           })
       },
-      goToAdmin(id) {
-        this.$router.push(`/admin/${id}`)
+      goToAdmin(row) {
+        this.$router.push(`/admin/${row.id}`)
       },
     },
     components: {}
   }
 </script>
 
-<style>
-
+<style scoped>
+  .table-container {
+    width: 50%;
+    margin: 0 auto;
+    padding-top: 20px;
+  }
+  .table {
+    text-align: center;
+  }
 </style>
